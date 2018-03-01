663a8c435d27b10a863c2fda674015bdf10d5225
Tony Kelman <tony@kelman.net>
2018-03-01 13:25:39 -0800

    more debug output

--- recipe/post-link.bat
+++ recipe/post-link.bat
@@ -1,4 +1,6 @@
 @echo off
 
 :: Install kernelspec at post-link because conda doesn't substitute Windows paths correctly in JSON files
-"%PREFIX%"\Python.exe -m ipykernel install --sys-prefix > NUL 2>&1 && if errorlevel 1 exit 1
+type "%PREFIX%"\share\jupyter\kernels\python2\kernel.json
+"%PREFIX%"\Python.exe -m ipykernel install --sys-prefix 2>&1 && if errorlevel 1 exit 1
+type "%PREFIX%"\share\jupyter\kernels\python2\kernel.json
